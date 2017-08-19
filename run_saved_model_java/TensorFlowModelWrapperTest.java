d6dc065780a5f029c7e30312a60526010bbdae2b
mdn <marhlder@gmail.com>
2017-08-19 23:44:40 +0200

    Cleanup of JAVA code for running parsey model

--- run_saved_model_java/TensorFlowModelWrapperTest.java
+++ run_saved_model_java/TensorFlowModelWrapperTest.java
@@ -2,17 +2,12 @@ import javafx.util.Pair;
 import org.junit.Test;
 import org.tensorflow.*;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 class ParseyMcParsefaceWrapper extends TensorflowModelWrapper<Tensor> {
 
@@ -87,7 +82,7 @@ public class TensorFlowModelWrapperTest {
 
 
             final Tensor mock_input = Tensor.create(inputString.getBytes(StandardCharsets.UTF_8));
-            System.out.println("INPUT STRING IS:\n" + inputString);
+            //System.out.println("INPUT STRING IS:\n" + inputString);
 
             final List<Pair<String, Tensor>> inputs = Arrays.asList(
                     new Pair<>("input", mock_input)
@@ -95,55 +90,18 @@ public class TensorFlowModelWrapperTest {
 
             final List<Tensor> outputs = rrnWrapper.runModel(inputs);
 
-            //final ByteBuffer dstBuffer = ByteBuffer.allocate(1000);
-            //dstBuffer.clear();
-
             for (int counter = 0; counter < outputs.size(); counter++) {
 
                 final Tensor tensor = outputs.get(counter);
+                final byte[] out_bytes = tensor.bytesValue();
 
-                //System.out.println("numdimensions: " + tensor.numDimensions());
-                //System.out.println("numdimensions: " + tensor.numElements());
-                //System.out.println("Tensor type: " + tensor.dataType().name());
-
-                //System.out.println("shape length = " + outputs.get(counter).shape().length);
-                //System.out.println("shape value = " + outputs.get(counter).shape()[0]);
-                byte[] out_bytes = tensor.bytesValue();
-
-                //String v = Charset.forName("UTF-8").decode(ByteBuffer.wrap(out_bytes)).toString();
-
-                //byte[] out_bytes = new byte[dstBuffer.capacity() - dstBuffer.remaining()];
-                //dstBuffer.rewind();
-                //dstBuffer.get(out_bytes);
-
-                /*for (byte by : out_bytes) {
-                    System.out.println(by);
-                }*/
-
-                System.out.println("out_bytes length" + out_bytes.length);
-                System.out.println("Bytes:\n" + bytesToHex(out_bytes));
-
-                String string = new String(out_bytes, StandardCharsets.UTF_8);
-                System.out.println(string);
-
-                //dstBuffer.clear();
+                final String string = new String(out_bytes, StandardCharsets.UTF_8);
+                //System.out.println(string);
             }
         }
     }
 
-    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
-
-    static String readFile(String path, Charset encoding)
+    private static String readFile(final String path, final Charset encoding)
             throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, encoding);
