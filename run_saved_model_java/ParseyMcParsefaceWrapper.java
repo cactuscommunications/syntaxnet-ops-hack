74fbc1bb192336559c580be451d21f4eee1fae9a
Unknown <marhlder@gmail.com>
2017-08-24 13:45:52 +0200

    Fix Tensor leak
    Turns out you need to close the Tensors to free their native memory

--- run_saved_model_java/ParseyMcParsefaceWrapper.java
+++ run_saved_model_java/ParseyMcParsefaceWrapper.java
@@ -20,37 +20,64 @@ public class ParseyMcParsefaceWrapper extends TensorflowModelWrapper<String, Str
     @Override
     protected String runModelImpl(final String input, final Session session) {
 
-        final int ONE_MB = 1024 * 1024;
-        final Splitter splitter = Splitter.fixedLength(ONE_MB);
+        final int ONE_MB = 1024 * 128;
+        final int CHUNK_SIZE = ONE_MB;
+        final Splitter splitter = Splitter.on('\n').trimResults().omitEmptyStrings();
 
         // User information...
-        System.out.println("Fetching session...");
+        System.out.println("Fetching session... Input is: " + input.length());
 
         // Retrieve runner from session
-        final Session.Runner runner = session.runner();
-
+        final StringBuilder inputBuilder = new StringBuilder();
         final StringBuilder outputBuilder = new StringBuilder();
 
-        for (String substring : splitter.splitToList(input)) {
+        // Build chunks of CHUNK_SIZE and feed them to parsey
+        for (String substring : splitter.split(input)) {
 
-            // Create input Tensor from String input
-            final Tensor inputTensor = Tensor.create(substring.getBytes(StandardCharsets.UTF_8));
+            if (inputBuilder.length() + substring.length() >= CHUNK_SIZE - 1) {
 
-            // Feed all inputs to the graph
-            runner.feed("input", inputTensor);
+                final String output = runParseyMcParseface(inputBuilder.toString(), session);
+                outputBuilder.append(output);
+                inputBuilder.setLength(0);
+            }
 
-            // Fetch all graph outputs
-            final Tensor outputTensor = runner.fetch(this.outputOpNames.get(0)).run().get(0);
+            inputBuilder.append(substring);
+            inputBuilder.append('\n');
+        }
 
-            // Fetch output bytes from tensor:
-            final byte[] outputBytes = outputTensor.bytesValue();
-
-            // Decode bytes to String
-            final String output = new String(outputBytes, StandardCharsets.UTF_8);
-
+        // Do a final run on the remaining text
+        if(inputBuilder.length() > 0)
+        {
+            final String output = runParseyMcParseface(inputBuilder.toString(), session);
             outputBuilder.append(output);
         }
 
         return outputBuilder.toString();
+    }
+
+    private String runParseyMcParseface(final String input, final Session session)
+    {
+        System.out.println("About to run on " + input.length() + " chars");
+
+        final Session.Runner runner = session.runner();
+
+        // Create input Tensor from String input
+        final Tensor inputTensor = Tensor.create(input.getBytes(StandardCharsets.UTF_8));
+
+        // Feed all inputs to the graph
+        runner.feed("input", inputTensor);
+
+        // Fetch all graph outputs
+        final Tensor outputTensor = runner.fetch(this.outputOpNames.get(0)).run().get(0);
+
+        // Fetch output bytes from tensor:
+        final byte[] outputBytes = outputTensor.bytesValue();
+
+        // Close tensors (i.e. free native memory)
+        outputTensor.close();
+        inputTensor.close();
+
+        // Decode bytes to String
+        return new String(outputBytes, StandardCharsets.UTF_8);
     }
 }
--- run_saved_model_java/TensorFlowModelWrapperTest.java
+++ run_saved_model_java/TensorFlowModelWrapperTest.java
@@ -18,35 +18,48 @@ public class TensorFlowModelWrapperTest {
 
         final String output;
 
-        try (final ParseyMcParsefaceWrapper rrnWrapper =
-                     new ParseyMcParsefaceWrapper(
-                             "/mnt/c/Users/marhl/syntax_net_with_tensors/models/syntaxnet/SAVED_MODEL/")) {
+        final String inputString =
+                readFile("/mnt/c/Users/marhl/unsilo/data/springer/merged-test-data.txt", StandardCharsets.UTF_8);
 
-            System.out.println("Model loaded...");
+        /*final String inputString =
+                    readFile("/mnt/c/Users/marhl/jni_with_ops_hack/tensorflow/test_data/test_document.txt", StandardCharsets.UTF_8);*/
 
-            /*final String[] mockdata =
-                    {
+        /*final String[] mockdata =
+                {
+                            "Mr. Johnson was too late to a nano,party.",
                             "I love grapes.",
                             "Do you even lift bro?",
                             "Does your mother known that your out?",
                             "If you want to remove specific punctuation from a string, it will probably be best to explicitly remove exactly what you want like."
-                    }; //*/
+                }; //
 
-            /*final StringBuffer stringBuffer = new StringBuffer();
+        final StringBuffer stringBuffer = new StringBuffer();
 
-            for (int counter = 0; counter < mockdata.length; counter++) {
-                stringBuffer.append(mockdata[counter]);
-                stringBuffer.append('\n');
-            }*/
+        for (int counter = 0; counter < mockdata.length; counter++) {
+            stringBuffer.append(mockdata[counter]);
+            stringBuffer.append('\n');
+        }
 
-            //final String inputString = stringBuffer.toString();
-            final String inputString =
-                    readFile("/mnt/c/Users/marhl/jni_with_ops_hack/tensorflow/test_data/test_document.txt", StandardCharsets.UTF_8);
+        final String inputString = stringBuffer.toString();*/
 
+        final long timeBefore;
+        final long timeAfter;
+
+        try (final ParseyMcParsefaceWrapper rrnWrapper =
+                     new ParseyMcParsefaceWrapper(
+                             "/mnt/c/Users/marhl/syntax_net_with_tensors/models/syntaxnet/SAVED_MODEL/")) {
+
+            //System.out.println("Model loaded...");
+            timeBefore = System.currentTimeMillis();
             output = rrnWrapper.runModel(inputString);
+            timeAfter = System.currentTimeMillis();
         }
 
         System.out.println(output);
+        System.out.println("INPUT LENGTH" + inputString.length());
+        System.out.println("OUT LENGTH: " + output.length());
+        System.out.println("Time to run model: " + (timeAfter - timeBefore) );
+
     }
 
     private static String readFile(final String path, final Charset encoding)
